e738e2e6d689b365795004e79de85661ca32d334
Safaumer <su9991@g.rit.edu>
2024-02-12 14:04:06 +0400

    test file for correct

---
+++ correct_test.py
@@ -0,0 +1,13 @@
+import turtle
+def test():
+    actual="x"
+    fake=pixel()
+    assert actual==fake
+
+def pixel():
+    while i==(4):
+        while w==(4):
+            turtle.forward(30)
+            turtle.right(90)
+        turtle.forward(30)
+    return "x"
