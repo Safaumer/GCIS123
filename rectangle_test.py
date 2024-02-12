30b5e473ac17db00aaebd461c6cbc7fad6cdd492
Safaumer <su9991@g.rit.edu>
2024-02-12 14:05:32 +0400

    test for rectangle

---
+++ rectangle_test.py
@@ -0,0 +1,11 @@
+ 
+def draw_row(row,height,width,color):
+    for a in range(row):
+        draw_pixel(height,width,color)
+        new_pixel(width)
+        return x
+    
+def test():
+    y=draw_pixel("red")
+    assert x=="y"
+
